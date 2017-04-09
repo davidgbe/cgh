d992c7bc1e58e50f0bf9a5cba12720af0306ca47
David Bell <davidgbe@usc.edu>
2017-04-09 11:06:27 -0700

    strip matplotlib

--- lib/utilities.py
+++ lib/utilities.py
@@ -1,6 +1,5 @@
 import numpy as np
 import os
-import matplotlib.pyplot as plt
 import multiprocessing as mp
 
 def to_grayscale(recon, original):
@@ -10,19 +9,19 @@ def file_path(curr_file, *path_elements):
     dir = os.path.dirname(curr_file)
     return os.path.join(dir, *path_elements)
 
-def save_plot(name):
-    plt.savefig(file_path(__file__, '../images/%s.png' % name))
+# def save_plot(name):
+#     plt.savefig(file_path(__file__, '../images/%s.png' % name))
 
-def save_image(image_data, name):
-    plt.imshow(image_data, interpolation='nearest', cmap='gray')
-    save_plot(name)
+# def save_image(image_data, name):
+#     plt.imshow(image_data, interpolation='nearest', cmap='gray')
+#     save_plot(name)
 
-def save_scatter(name, Y, X=None):
-    if X is None:
-        X = [i for i in range(len(Y))]
-    plt.plot(X, Y, 'ro')
-    save_plot(name)
-    plt.clf()
+# def save_scatter(name, Y, X=None):
+#     if X is None:
+#         X = [i for i in range(len(Y))]
+#     plt.plot(X, Y, 'ro')
+#     save_plot(name)
+#     plt.clf()
 
 def bucket(data, bucket_size):
     return [ np.mean(data[i:i+bucket_size]) for i in range(0, len(data), bucket_size) ]
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from lib.mnist import MNIST
-from lib.utilities import save_image
+#from lib.utilities import save_image
 from lib.real_object import RealObject
 import numpy as np
 import multiprocessing as mp
@@ -7,10 +7,7 @@ import multiprocessing as mp
 mp.freeze_support()
 image = MNIST.get_image()
 
-save_image(image, 'original')
-
 ro = RealObject(image, np.zeros(3), 1, 1)
 
 holo = ro.generate_interference_pattern(np.array([4, 0, 0]), 1, 1)
 print(holo.image)
-save_image(holo.image, 'holo')

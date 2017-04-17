cf12580f7d707c690773c3feec38be6740f01062
David Bell <davidgbe@usc.edu>
2017-04-16 20:57:28 -0700

    modify for cl args

--- run.py
+++ run.py
@@ -7,13 +7,22 @@ import pickle
 import sys
 from random import randint as rand
 
+args = sys.argv
+write_path = args[1]
+output_name = args[2]
+depth = float(args[3])
+displacement = float(args[4])
+sampling_frequency = float(args[5])
+holo_resolution = float(args[6])
+
+
 mp.freeze_support()
 image = MNIST.get_image()
 
 ro = RealObject(image, np.zeros(3), 1, 1)
 print(sys.argv)
-holo = ro.generate_interference_pattern(np.array([4, 0, 0]), 1, 1)
+holo = ro.generate_interference_pattern(np.array([depth, displacement, 0]), 1, 1, sampling_size=sampling_frequency, holo_scale=holo_resolution)
 print(holo.image)
-file = open(sys.argv[1] + ("/img_%s.p" % sys.argv[2]), 'wb')
+file = open(write_path + ("/img_%s.p" % output_name), 'wb')
 pickle.dump(holo.image, file)
 file.close()
